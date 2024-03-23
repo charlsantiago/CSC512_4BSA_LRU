0b2bde7b77fe6a31f008b9e036d04121d1bdd64c
charlsantiago <77201252+charlsantiago@users.noreply.github.com>
2024-03-23 15:00:11 +0800

    Update Cache_Algorithm.py

--- Cache_Algorithm.py
+++ Cache_Algorithm.py
@@ -12,8 +12,11 @@ class Cache_Algorithm:
         self.memory = list(range(self.memory_blocks))
         self.trace = [] 
         self.access_count = 0
+        self.hit_rate = 0
+        self.miss_rate = 0
         self.hit_count = 0
         self.miss_count = 0
+        self.avg_access_time = 0
         self.total_access_time = 0
         
         self.cache = {i: {'blocks': ['X'] * self.associativity, 'lru': list(range(self.associativity))}
@@ -24,21 +27,11 @@ class Cache_Algorithm:
         print("-----------------------------")
         data_arr=[]
         snapshot = f"Step: {step}, Current Cache Memory:\n"
-        print (snapshot)
-        #print(snapshot)
         if current_block is not None:
             snapshot += f"Block {current_block}\n"
-            #print(snapshot)
-        #snapshot += "---------------------\n"
-        #snapshot += "| Cache Index | Data |\n"
-        #snapshot += "---------------------\n"
         for index, data in enumerate(self.cache.values()):
-            #snapshot += f"| {index:12d} | {', '.join(map(str, data['blocks']))} |\n"
-            #print(data['blocks'])
             data_arr.append(data['blocks'])
-            #print(snapshot)
         age_df = pd.DataFrame(data = np.array(data_arr), columns = ['Block 0', 'Block 1', 'Block 2', 'Block 3'] )
-        print(age_df)
         snapshot+= age_df.to_string()
         snapshot += "\n---------------------\n\n"
 
@@ -46,27 +39,25 @@ class Cache_Algorithm:
 
 
     def statistics(self):
-        hit_rate = self.hit_count / self.access_count * 100 if self.access_count > 0 else 0
-        miss_rate = self.miss_count / self.access_count * 100 if self.access_count > 0 else 0
+        self.hit_rate = self.hit_count / self.access_count * 100 if self.access_count > 0 else 0
+        self.miss_rate = self.miss_count / self.access_count * 100 if self.access_count > 0 else 0
         miss_penalty = 22
-        #avg_access_time = 1 + 4 * (miss_rate / 100)
         avg_access_time_hit = (1 * (self.hit_count / self.access_count)) 
         avg_access_time_miss = (miss_penalty * (self.miss_count / self.access_count))
-        avg_access_time = avg_access_time_hit + avg_access_time_miss
+        self.avg_access_time = avg_access_time_hit + avg_access_time_miss
 
         total_access_time_hit = (2 * self.hit_count * 1) 
         total_access_time_miss = (self.miss_count * 1) + (self.miss_count * 2 * (1 + 10))
-        total_access_time = total_access_time_hit + total_access_time_miss
-        #total_access_time = self.access_count * avg_access_time
+        self.total_access_time = total_access_time_hit + total_access_time_miss
 
         self.trace.append(f"Simulation Statistics:\n")
         self.trace.append(f"1. Memory Access Count: {self.access_count}\n")
         self.trace.append(f"2. Cache Hit Count: {self.hit_count}\n")
         self.trace.append(f"3. Cache Miss Count: {self.miss_count}\n")
-        self.trace.append(f"4. Cache Hit Rate: {hit_rate:.2f}%\n")
-        self.trace.append(f"5. Cache Miss Rate: {miss_rate:.2f}%\n")
-        self.trace.append(f"6. Average Memory Access Time: {avg_access_time:.2f} cycles\n")
-        self.trace.append(f"7. Total Memory Access Time: {total_access_time:.2f} cycles\n")
+        self.trace.append(f"4. Cache Hit Rate: {self.hit_rate:.2f}%\n")
+        self.trace.append(f"5. Cache Miss Rate: {self.miss_rate:.2f}%\n")
+        self.trace.append(f"6. Average Memory Access Time: {self.avg_access_time:.2f} cycles\n")
+        self.trace.append(f"7. Total Memory Access Time: {self.total_access_time:.2f} cycles\n")
 
 
     def memory_access(self, block):
@@ -74,14 +65,12 @@ class Cache_Algorithm:
         cache_index = block % self.cache_blocks
         if block in self.cache[cache_index]['blocks']:
             print(f"Sequence: {block} | HIT | Set: {cache_index}")
-            #snapshot += f"Sequence: {block} | HIT | Set: {cache_index}"
             self.hit_count += 1
             block_index = self.cache[cache_index]['blocks'].index(block)
             self.cache[cache_index]['lru'].remove(block_index)
             self.cache[cache_index]['lru'].append(block_index)
         else:
             print(f"Sequence: {block} | MISS | Set: {cache_index}")
-            #snapshot += f"Sequence: {block} | MISS | Set: {cache_index}"
             self.miss_count += 1
             if len(self.cache[cache_index]['blocks']) < self.associativity:
                 self.cache[cache_index]['blocks'][0] = block
@@ -108,11 +97,9 @@ class Cache_Algorithm:
         if test_case == 'Sequential':
             input = 63
             sequence = list(range(input + 1)) * 4
-            #sequence = list(range(2 * self.cache_blocks)) * 4
         elif test_case == 'Random':
             for i in range (128):
                 sequence.append(randrange(128))
-            #sequence = sample(range(4 * self.cache_blocks), 4 * self.cache_blocks)
         elif test_case == 'Mid-Repeat':
             mid = []
             for i in range(28):
@@ -121,12 +108,6 @@ class Cache_Algorithm:
             for i in range(32):
                  end.append(32 + i)
             sequence = ([0,1,2,3] + mid + mid + end) *4
-            #sequence = list(range(self.cache_blocks)) * 2 + list(range(2 * self.cache_blocks)) * 2
         else:
             raise ValueError("Invalid test case")
-        print(sequence)
         return sequence
-
-
-
-
